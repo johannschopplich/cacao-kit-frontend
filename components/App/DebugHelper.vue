7ffde4909d180869fd5322f6db2da16a2362e8b2
Johann Schopplich <mail@johannschopplich.com>
2023-03-27 20:47:41 +0200

    refactor: outsource debug helper

---
+++ components/App/DebugHelper.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { FetchError } from 'ofetch'
+
+defineProps<{
+  error?: FetchError
+}>()
+
+const { debug } = useRuntimeConfig().public
+</script>
+
+<template>
+  <details v-if="debug && error">
+    <summary>Error fetching page data</summary>
+    <pre style="font-size: 0.875em">{{
+      JSON.stringify(error, undefined, 2)
+    }}</pre>
+  </details>
+</template>
