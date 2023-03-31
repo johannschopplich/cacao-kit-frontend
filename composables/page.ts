2400c90ce5cc3c05af852147603680eaeddd26e4
Johann Schopplich <mail@johannschopplich.com>
2023-03-31 09:27:53 +0200

    feat: add `isPageReady` composable alias

--- composables/page.ts
+++ composables/page.ts
@@ -9,14 +9,14 @@ export function useKirbyStaticData() {
 }
 
 /**
- * Returns the currently active page
+ * Returns the currently active page, similar to Kirby's `$page` global variable
  */
 export function usePage<T extends Record<string, any> = Record<string, any>>() {
   return useState<T>('app.page', () => ({} as T))
 }
 
 /**
- * Sets the currently active page
+ * Sets the currently active page and updates the document head
  */
 export function setPage<T extends Record<string, any>>(page?: T) {
   if (!page) return
@@ -69,10 +69,18 @@ export function setPage<T extends Record<string, any>>(page?: T) {
  * Returns a promise that resolves when the page data has been loaded
  */
 export async function hasPage() {
-  await until(usePageState()).toBe('ready')
+  const state = usePageState()
+  if (state.value === 'ready') return true
+
+  await until(state).toBe('ready')
   await nextTick()
   return true
 }
+
+/**
+ * Alias for `hasPage()`
+ */
+export const isPageReady = hasPage
 
 function usePageState() {
   return useState('app.state.page', () => 'pending')
--- composables/site.ts
+++ composables/site.ts
@@ -1,7 +1,7 @@
 import type { KirbySite } from '#build/kql'
 
 /**
- * Access the global site context
+ * Access the global site context, similar to Kirby's `$site` global variable
  */
 export function useSite() {
   return useState<Partial<KirbySite>>('app.site', () => ({}))
