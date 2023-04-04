106c7a0ad4aeb34f67b08dda9ab4905709a091bc
Johann Schopplich <mail@johannschopplich.com>
2023-04-04 19:17:14 +0200

    refactor: improve `KirbyUuidResolver` component

--- components/Kirby/UuidResolver.ts
+++ components/Kirby/UuidResolver.ts
@@ -1,12 +1,20 @@
-interface UuidResolverOptions {
-  uuid: string
-  collection: ({ uuid: string } & Record<string, any>)[]
-}
+import type { PropType } from 'vue'
+
+type UuidResolverCollection = (Record<string, any> & { uuid: string })[]
 
 // Resolve a given UUID to an item in a collection, especially useful for
 // files that are not resolved server-side
-export default /* #__PURE__ */ defineComponent<UuidResolverOptions>({
-  props: ['uuid', 'collection'] as unknown as undefined,
+export default defineComponent({
+  props: {
+    uuid: {
+      type: String,
+      required: true,
+    },
+    collection: {
+      type: Array as PropType<UuidResolverCollection>,
+      required: true,
+    },
+  },
   setup(props, { slots }) {
     const data = reactive({
       item: resolveUuid(props.collection, props.uuid),
