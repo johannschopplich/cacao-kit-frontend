7648ed09b5cd54c9e8d20d7a4c3a50820898d6bd
Johann Schopplich <mail@johannschopplich.com>
2023-05-22 17:24:43 +0200

    fix: filter empty route values before removing locale prefix

--- utils/locale.ts
+++ utils/locale.ts
@@ -4,12 +4,14 @@ export function getNonLocalizedSlug(param: string | string[]) {
   const { languages } = kirbyStatic
 
   if (Array.isArray(param)) {
+    param = param.filter(Boolean)
+
     // If the first item is a locale, remove it
     if (param.length > 0 && languages.includes(param[0])) {
       param = param.slice(1)
     }
 
-    return param.filter(Boolean).join('/')
+    return param.join('/')
   }
 
   return param
