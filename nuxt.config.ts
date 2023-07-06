0da41e6ebd671570a7868a81bf4e20f337a8c743
Johann Schopplich <mail@johannschopplich.com>
2023-07-06 08:32:47 +0200

    chore: ignore redirect uri for prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,6 +46,8 @@ export default defineNuxtConfig({
       ...(shouldPrerender && {
         crawlLinks: shouldPrerender,
         routes: ['/en'],
+        // Ignore homepage, which redirects to the user's locale
+        ignore: ['/'],
       }),
     },
   },
