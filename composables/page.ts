9bf47edda44490811924af6e9b5517a727b868b8
Johann Schopplich <mail@johannschopplich.com>
2023-07-14 08:45:03 +0200

    fix: add missing meta description

--- composables/page.ts
+++ composables/page.ts
@@ -54,6 +54,7 @@ export function setPage<T extends Record<string, any>>(page?: T) {
 
   useSeoMeta({
     title,
+    description,
     ogTitle: title,
     ogDescription: description,
     ogUrl: url,
