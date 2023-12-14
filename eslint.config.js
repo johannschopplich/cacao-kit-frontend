f496a66238dbc7db25d3c096da2547335b246ed2
Johann Schopplich <mail@johannschopplich.com>
2023-12-14 09:55:01 +0100

    chore: upgrade dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,20 @@
 import antfu from '@antfu/eslint-config'
-import unocss from '@unocss/eslint-config/flat'
 
-export default [
-  unocss,
-  ...(await antfu(
-    {
-      stylistic: false,
-      ignores: ['tsconfig.json'],
+export default await antfu(
+  {
+    stylistic: false,
+    unocss: true,
+    ignores: ['tsconfig.json'],
+  },
+  {
+    rules: {
+      'node/prefer-global/buffer': 'off',
+      'node/prefer-global/process': 'off',
+      // Ignore rules clashing with Prettier
+      'vue/html-closing-bracket-newline': 'off',
+      'vue/html-indent': 'off',
+      'vue/html-self-closing': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
     },
-    {
-      rules: {
-        'node/prefer-global/buffer': 'off',
-        'node/prefer-global/process': 'off',
-        // Ignore rules clashing with Prettier
-        'vue/html-closing-bracket-newline': 'off',
-        'vue/html-indent': 'off',
-        'vue/html-self-closing': 'off',
-        'vue/singleline-html-element-content-newline': 'off',
-      },
-    },
-  )),
-]
+  },
+)
