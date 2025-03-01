3171f60b3183a55b037025dadaeaa8ec1a0d857a
Johann Schopplich <mail@johannschopplich.com>
2025-03-01 15:04:02 +0100

    chore: remove prerendering configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 import process from 'node:process'
-import { consola } from 'consola'
 import { prefetchQuery, siteQuery } from './queries'
-
-const shouldPrerender = process.env.NITRO_PRERENDER_PAGES === 'true'
-if (shouldPrerender) consola.info('Prerendering enabled')
 
 export default defineNuxtConfig({
   // If you project doesn't require i18n, use the `without-i18n` branch instead:
@@ -55,17 +51,6 @@ export default defineNuxtConfig({
     bundle: {
       fullInstall: false,
       dropMessageCompiler: true,
-    },
-  },
-
-  nitro: {
-    prerender: {
-      // Enable Nitro's crawler to prerender all pages (optional)
-      // If Kirby content changes, the frontend will have to be rebuilt
-      ...(shouldPrerender && {
-        crawlLinks: shouldPrerender,
-        routes: ['/en'],
-      }),
     },
   },
 
